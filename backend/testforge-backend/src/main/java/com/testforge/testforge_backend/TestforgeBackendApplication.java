198cb82ce88c401bc51c15e10ac8b56557bd495c
Faris Johari <faris.johari.tech@gmail.com>
2026-09-11 11:07:22 +0800

    chore: initialize TestForge

---
+++ backend/testforge-backend/src/main/java/com/testforge/testforge_backend/TestforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.testforge.testforge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestforgeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/testforge-backend/src/test/java/com/testforge/testforge_backend/TestforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testforge.testforge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
